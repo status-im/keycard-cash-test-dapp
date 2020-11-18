3a76456068332818b030daf145b4e798c8990e6d
Andrea Franz <andrea@gravityblast.com>
2020-11-18 17:38:47 +0100

    dist

---
+++ precache-manifest.d8bfb0d872b22980bf6f8f603f09ad17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14efc94cfbe9c3e2bffa275bbc3b7b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4665157d0a78d7639f8",
+    "url": "./static/css/main.92ffb5bd.chunk.css"
+  },
+  {
+    "revision": "4077ecf7137b6ea3a914",
+    "url": "./static/js/2.51cbcca5.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "./static/js/2.51cbcca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4665157d0a78d7639f8",
+    "url": "./static/js/main.6f668041.chunk.js"
+  },
+  {
+    "revision": "9af6804656af60742e66",
+    "url": "./static/js/runtime-main.137d6904.js"
+  }
+]);
